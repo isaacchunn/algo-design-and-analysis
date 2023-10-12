b3c8e40c9952c9ae9407450868d6407f021e96c1
Isaac <isaacchunn@hotmail.com>
2023-10-13 04:06:16 +0800

    Add in graph framework and removed old dijkstra's implementation

--- Graph Framework/Dijkstra/ListNode.h
+++ Graph Framework/Dijkstra/ListNode.h
@@ -1,10 +1,16 @@
 #ifndef LISTNODE_H
 #define LISTNODE_H
+
+#include "Node.h"
+
 //Represent the costs of the graph in adjacency list
 struct ListNode
 {
-    //Associated node
+	//Associated node for identification purposes
+	Node* node;
+	//Weight to this node
 	int cost;
+	//Next node
 	ListNode* next;
 };
 #endif
